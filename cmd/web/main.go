e9ab47e19b28ba45706a912507345fa4382fa4cc
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-24 22:50:40 +0300

    implemented the static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// creating our file server for static files
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// using Handle() to register the file server as the handler for
+	// all URL paths that start with "/static/"
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// we can serve single files with http.ServeFile()
+	// but that's more unsafe and less efficient
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)

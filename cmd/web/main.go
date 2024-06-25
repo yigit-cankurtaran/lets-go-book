19eb9ce4b5422f8228d29211f1c6bce85b17ad34
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-25 14:20:42 +0300

    implemented command line flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,21 @@ package main
 // in the project. we'll have 1 exec so far, will live here
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// command line flag named addr, default is ":4000"
+	// and some short text to explain what it controls
+	// having these values hardcoded isn't ideal
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// we use String because if the val can't be an str
+	// it will log an error and exit
+	flag.Parse()
+	// we need to call Parse before we use the addr var
+
 	mux := http.NewServeMux()
 
 	// creating our file server for static files
@@ -21,10 +31,12 @@ func main() {
 	// but that's more unsafe and less efficient
 
 	mux.HandleFunc("/", home)
+	// HandleFunc transforms a function into a handler
+	// and registers it in the same step
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }

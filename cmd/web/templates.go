c25ac8d678d57882112af2550f4b830a8e75f29c
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-28 19:11:18 +0300

    automatically parsing partials

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -14,37 +14,30 @@ type templateData struct {
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
-	// initialize map to act as the cache
 	cache := map[string]*template.Template{}
-
-	// use filepath.Glob to get a slice of all file paths in the pages
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
 	if err != nil {
 		return nil, err
 	}
 
-	// loop through the pages slice
 	for _, page := range pages {
-		// get file name and assign it to name
 		name := filepath.Base(page)
 
-		// create slice for filepaths
-		files := []string{
-			"./ui/html/base.tmpl.html",
-			"./ui/html/partials/nav.tmpl.html",
-			page,
-		}
-
-		// parse files into template set
-		ts, err := template.ParseFiles(files...)
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
 
-		// add the template set to the map using name as key
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
 		cache[name] = ts
 	}
-
-	// return the map
 	return cache, nil
 }
